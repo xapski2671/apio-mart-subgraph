d7b1b3ff8a4c5fb36598b65cb8bb15423fb74bac
xapski <xapski2671@gmail.com>
2022-08-09 20:50:44 +0100

    Initial commit

---
+++ tests/nft-marketplace.test.ts
@@ -0,0 +1,66 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { ExampleEntity } from "../generated/schema"
+import { CollectionFound } from "../generated/NFTMarketplace/NFTMarketplace"
+import { handleCollectionFound } from "../src/nft-marketplace"
+import { createCollectionFoundEvent } from "./nft-marketplace-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let name_ = "Example string value"
+    let symbol_ = "Example string value"
+    let nftAddress_ = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let newCollectionFoundEvent = createCollectionFoundEvent(
+      name_,
+      symbol_,
+      nftAddress_
+    )
+    handleCollectionFound(newCollectionFoundEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "name_",
+      "Example string value"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "symbol_",
+      "Example string value"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "nftAddress_",
+      "0x0000000000000000000000000000000000000001"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
